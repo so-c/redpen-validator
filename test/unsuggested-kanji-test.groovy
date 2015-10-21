5c47697dceb355f9e6ccffe473215ecd7b075787
so-c <so_c321@hotmail.co.jp>
2015-10-22 00:25:43 +0900

    add 事 check test.

--- test/unsuggested-kanji-test.groovy
+++ test/unsuggested-kanji-test.groovy
@@ -2,21 +2,53 @@ package soc.com.github.redpenvalidator
 
 import org.junit.Test
 import static org.junit.Assert.*
-import cc.redpen.config.ValidatorConfiguration
+import static org.hamcrest.CoreMatchers.*
+
+import cc.redpen.RedPen;
 import cc.redpen.validator.JavaScriptValidator
 
+import cc.redpen.config.ValidatorConfiguration
+import cc.redpen.config.Configuration
+
+import cc.redpen.parser.DocumentParser
+import cc.redpen.parser.SentenceExtractor;
+
 /**
- * Precondition: add redpen-core-1.4.jar, slf4j-api-1.7.6.jar to ClassPath from REDPEN_HOME\lib
+ * Precondition: add following jars to ClassPath from REDPEN_HOME\lib:
  *               run in home of this repository.
  */
 class UnsuggestedKanjiTest {
+  def vc = new ValidatorConfiguration("JavaScript")
+              .addAttribute("script-path", System.getProperty("user.dir") + /\src/)
+  def conf = new Configuration.ConfigurationBuilder()
+                              .addValidatorConfig(vc)
+                              .setLanguage("ja")
+                              .build()
+
   @Test
   void test_javascript_validators_are_loaded() {
-    def vc = new ValidatorConfiguration("JavaScriptValidator")
-                .addAttribute("script-path", System.getProperty("user.dir") + /\src/)
-    JavaScriptValidator sut = new JavaScriptValidator()
+    // setup
+    def sut = new JavaScriptValidator()
     sut.preInit(vc, null)
     
-    assertEquals(1, sut.scripts.size())
+    // verify
+    assertThat sut.scripts.size(), is(1)
+  }
+  
+  @Test 
+  void test_check_事() {
+    // setup
+    def parser = DocumentParser.PLAIN
+    def doc = parser.parse("知らない事がある。", 
+                           new SentenceExtractor(conf.getSymbolTable()),
+                           conf.getTokenizer())
+     
+    // exercise
+    def errors = (new RedPen(conf)).validate([doc]).get(doc)
+     
+    // verify
+    assertThat errors.size(), is(1)
+    assertThat errors.get(0).getMessage(), is("[unsuggested-kanji.js] 推奨しない漢字が「事」で使われています。")
+
   }
 }
